b87c3317db78a6e2ce2831f0318fc52c11e0f986
David MOLINARI <contact@davidmolinari.fr>
2018-09-01 05:00:00 +0530

    Update Meh,

---
+++ meh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+list=("Amelioration du code", "Edit README", "Optimisation de l algorithme en nlog n", "Mise en place de decimation", "Passage sur version stable", "Amelioration de performances", "Mise a jours de la base de donnees", "Update Meh", "Meh", "Ajout de commentaires oublies" , "Factorisation du code !")
+for i in {1..29}
+do
+	touch temp$i.txt
+	git add -A
+	a="Mon Sep "
+	b=$i
+	c=" 5:00 2018 +0530"
+	export GIT_AUTHOR_DATE=$a$b$c
+	export GIT_COMMITTER_DATE=$a$b$c
+	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
+	rm temp$i.txt
+	export GIT_AUTHOR_DATE=$a$b$c
+	export GIT_COMMITTER_DATE=$a$b$c
+	export GIT_AUTHOR_NAME="Quentin Luppo"
+	export GIT_AUTHOR_EMAIL="quentinluppo@orange.fr"
+	export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+	export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+	git commit -m "Addes some patches"
+	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
+done
