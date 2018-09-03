5840e917eb2695d5f6da75ccc233abf242793139
RaimonDylan <dylanraimon@gmail.com>
2018-09-03 16:36:14 +0200

    use args

--- Day1/Mean/Outils.java
+++ Day1/Mean/Outils.java
@@ -1,7 +1,9 @@
 public class Outils{
 
 	public static void main(String[] args){
-		int[] tab = {10,20,14,9,15};
+		int[] tab = new int[args.length];
+		for (int i = 0; i < args.length; i++)
+    	tab[i] = Integer.parseInt(args[i]);
 		System.out.print(ConsoleColors.YELLOW+"Notes : "+ConsoleColors.RESET);
 		for (int note : tab )
 			System.out.print(note+" ");
