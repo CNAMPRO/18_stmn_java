6de357b8090d1741d7525e6da46ab2d4a5afe109
RaimonDylan <dylanraimon@gmail.com>
2018-09-03 16:31:32 +0200

    add colors

--- Day1/Mean/Outils.java
+++ Day1/Mean/Outils.java
@@ -2,12 +2,11 @@ public class Outils{
 
 	public static void main(String[] args){
 		int[] tab = {10,20,14,9,15};
-		System.out.print("Notes : ");
+		System.out.print(ConsoleColors.YELLOW+"Notes : "+ConsoleColors.RESET);
 		for (int note : tab )
 			System.out.print(note+" ");
 		System.out.println();
-
-		System.out.println("Moyenne : "+  moyenne(tab));
+		System.out.println(ConsoleColors.YELLOW+"Moyenne : "+ConsoleColors.RESET+  moyenne(tab));
 	}
 
 	public static int moyenne(int[] tab){
