35db955892e1a187a8cbad87c3bf57b34ead4d49
David MOLINARI <contact@davidmolinari.fr>
2018-09-03 16:43:00 +0200

    oups escroc

--- Day1/Mean/Mean.java
+++ Day1/Mean/Mean.java
@@ -1,18 +1,21 @@
-public class Mean {
-	public static void main(String[] args) {
-	    System.out.println("Calcul de la moyenne : 12, 15, 10, 0, 15, 20 =");
-      int[] a = {12, 15, 10, 0, 15, 20};
-      int result = moy(a);
-      System.out.println(result);
-    }
-		/*
-			Retourne la moyenne d'un tableau d'entiers
-		*/
-    public static int moy(int[] args){
-        int total = 0;
-    	  for(int i = 0; i<args.length;i++)
-           total+=args[i];
-      	return total/args.length;
-    }
+public class Mean{
 
- }
+	public static void main(String[] args){
+		int[] tab = new int[args.length];
+		for (int i = 0; i < args.length; i++)
+    	tab[i] = Integer.parseInt(args[i]);
+		System.out.print(ConsoleColors.YELLOW+"Notes : "+ConsoleColors.RESET);
+		for (int note : tab )
+			System.out.print(note+" ");
+		System.out.println();
+		System.out.println(ConsoleColors.YELLOW+"Moyenne : "+ConsoleColors.RESET+  moyenne(tab));
+	}
+
+	public static int moyenne(int[] tab){
+		int result = 0;
+		for (int note : tab )
+			result += note;
+		result = result/tab.length;
+		return result;
+	}
+}
