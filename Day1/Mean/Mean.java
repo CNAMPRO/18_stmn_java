1a6028ef1ca3a67d37002e65e9a51bffee64953a
David MOLINARI <contact@davidmolinari.fr>
2018-09-03 16:23:46 +0200

    Edit Mean.java

--- Day1/Mean/Mean.java
+++ Day1/Mean/Mean.java
@@ -1,18 +1,18 @@
-public class Mean {   
+public class Mean {
 	public static void main(String[] args) {
-	     System.out.println("Calcul de la moyenne : 12, 15, 10, 0, 15, 20 =\n");
+	    System.out.println("Calcul de la moyenne : 12, 15, 10, 0, 15, 20 =");
       int[] a = {12, 15, 10, 0, 15, 20};
       int result = moy(a);
       System.out.println(result);
     }
-
+		/*
+			Retourne la moyenne d'un tableau d'entiers
+		*/
     public static int moy(int[] args){
         int total = 0;
-    	   for(int i = 0; i<args.length;i++)
+    	  for(int i = 0; i<args.length;i++)
            total+=args[i];
-
-      return total/args.length;
+      	return total/args.length;
     }
 
  }
-
