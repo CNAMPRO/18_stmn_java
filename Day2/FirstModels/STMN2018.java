1e4c677208e1154b1916440c8b2289048a1d3c9b
Kevin BIGOt <bkevin8383@gmail.com>
2018-09-10 17:09:04 +0200

    Day 2

---
+++ Day2/FirstModels/STMN2018.java
@@ -0,0 +1,13 @@
+
+public class STMN2018 {
+	String[] eleves;
+	String[] professeurs;
+	
+	int nb_eleves, nb_professeurs;
+	
+	
+	static public void add_eleve(String eleve)
+	{
+		eleves[nb_eleves] = eleve;
+	}
+}
