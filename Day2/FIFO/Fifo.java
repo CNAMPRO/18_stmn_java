7028cecf974da53b10bf84a23fd513b8f6d3c571
adrian durando <adrian.d@noos.fr>
2018-09-10 12:01:07 +0200

    ouai

--- Day2/FIFO/Fifo.java
+++ Day2/FIFO/Fifo.java
@@ -2,19 +2,25 @@
 public class Fifo {
 	
 	int[]tab;
-	int cpt = 0;
+	int capacity;
+	int size;
+	
 	public static void main(String[] args)
 	{
 		
 	}
-	public static int pop()
+	public static int pop(Fifo f)
 	{
-		return tab
+		int head = f.tab[0];
+		for(int i=1;i<f.size;i++)
+			f.tab[i-1]=f.tab[i];
+		f.size = f.size -1;
+		return head;
 	}
-	public static void add(int value)
+	public static void add(Fifo f,int value)
 	{
-		tab[cpt] = value;
-		cpt++;
+		f.tab[f.size] = value;
+		f.size++;
 	}
 
 }
