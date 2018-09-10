72e7a26a217fa07f997ebd0f47e384235e23ac32
Asato <mickael.pierron7@orange.fr>
2018-09-10 08:52:40 +0200

    Exercice java

---
+++ matrice.java
@@ -0,0 +1,47 @@
+// one class needs to have a main() method
+public class Multiply
+{
+    public static void main(String[] args)
+    {
+        int[][] a = {{1,2},{3,4}};
+        int[][] b = {{4,3},{2,1}};
+        int[][] c = multiply(a,b);
+        afficheTab(c);
+    }
+
+    public static int[][] multiply(int[][] a, int[][] b){
+        if(a[0].length != b.length){
+            return null;
+        }
+        int lignes = (a.length * a[0].length < b.length * b[0].length)?b.length:a.length;
+        int col = (a.length * a[0].length < b.length * b[0].length)?b[0].length:a[0].length;
+        int[][] result =  new int[lignes][col];
+        lignes = 0;
+        for (int i = 0;i < a.length;i++){ 
+            col = 0;
+            for (int j = 0;j< b[0].length;j++){ 
+                int calcul= 0;
+                for (int k = 0;k < b.length;k++){  
+                    calcul += a[i][k] * b[k][j];
+                }
+                result[lignes][col] = calcul;
+                col++;
+            }
+            lignes++;
+        }
+        return result;
+    }
+    
+    public static void afficheTab(int [][]a){
+
+        for (int i = 0;i < a.length;i++)
+        {
+            for (int j = 0;j < a[0].length ;j++)
+            {
+                System.out.printf("%2d ",a[i][j] );
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
