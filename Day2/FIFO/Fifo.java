5a0ba5351384833f8683419ec6af84cb10274adc
adrian durando <adrian.d@noos.fr>
2018-09-10 11:45:56 +0200

    ouai

---
+++ Day2/FIFO/Fifo.java
@@ -0,0 +1,20 @@
+
+public class Fifo {
+	
+	int[]tab;
+	int cpt = 0;
+	public static void main(String[] args)
+	{
+		
+	}
+	public static int pop()
+	{
+		return tab
+	}
+	public static void add(int value)
+	{
+		tab[cpt] = value;
+		cpt++;
+	}
+
+}
