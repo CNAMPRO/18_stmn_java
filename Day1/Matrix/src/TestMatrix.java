925a91b01dfe8d5975607a92304e38935510ea99
David MOLINARI <contact@davidmolinari.fr>
2018-09-10 10:49:17 +0200

    Add Expected OutOfBoundException

--- Day1/Matrix/src/TestMatrix.java
+++ Day1/Matrix/src/TestMatrix.java
@@ -1,12 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
 
 class TestMatrix {
 
+	
+	@Rule
+	public final ExpectedException expected = ExpectedException.none();
+	
 	@Test
 	void test() {
-		
 		
 		//fail("Not yet implemented");
 	}
@@ -14,18 +20,23 @@ class TestMatrix {
 	@Test
 	void testMultiply() {
 		
-		//Double[][] A = {{1.00, 2.00},{3.00, 4.00}};
 		double[][] A = {{1.00, 2.00},{0.00, 3.00}};
 		double[][] B = {{1.00, 2.00, 3.00},{4.00, 5.00, 6.00}};
+		
 		double[][] C = Multiply.multiplicar(A, B);
-
 		double[][] R = {{9.00, 12.00, 15.00},{12.00, 15.00, 18.00}};
-
-		assertArrayEquals(R, C);
-
-		
-		
-		
+		assertArrayEquals(R, C);	
 		
 	}
+	
+	
+	@Test
+	void testMultiplyArrayEmpty(){
+		double[][] A = {};
+		double[][] B = {{1.00, 2.00, 3.00},{4.00, 5.00, 6.00}};
+
+		expected.expect(IndexOutOfBoundsException.class);
+		Multiply.multiplicar(A, B);
+	}
+
 }
