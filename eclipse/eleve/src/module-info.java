4ca8e6a391d2577f121b6d5be15f65f389acb433
Quentin Luppo <quentinluppo@orange.fr>
2018-09-10 15:25:49 +0200

    fifo

---
+++ eclipse/eleve/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author quent
+ *
+ */
+module eleve {
+}
---
+++ eclipse/eleve/src/promo.java
@@ -0,0 +1,14 @@
+
+public class promo {
+
+	String eleve;
+	String promo;
+	String prof;
+	String cour;
+	String salle;
+	public promo() {
+		System.out.println("Création promo");
+		promo="STMN 2018";
+		
+	}
+}
