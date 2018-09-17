2590bb7a34b631c4db6233207ded7378fa421f33
Asato <mickael.pierron7@orange.fr>
2018-09-17 15:05:31 +0200

    test

--- Day2/FIFO/src/FIFO.java
+++ Day2/FIFO/src/FIFO.java
@@ -1,15 +1,35 @@
 
 public class FIFO {
-	private int[] elements;
+	private int [] elements;
 	private int capacity;
 	private int size;
 	
-	FIFO(int capacity) {
+	Thread add = new add Thread();
+	add.start();
+	Thread pop = new pop Thread();
+	pop.start();
+	
+	Fifo(int capacity){
 		elements = new int[capacity];
 		this.capacity = capacity;
+		size = 0;
 	}
 	
-	public int pop() {
-		int head
+	public static int pop () {
+		int head = elements[0];
+		synchronize(elements){
+		for(int i = 1; i < f.size; i++) {
+			elements[i - 1] = elements[i]		
+		}
+		size--;
+		return head;
+		}
+	}
+	
+	public static void push (int valeur) {
+		elements[size] = valeur;
+		size++;
 	}
 }
+
+
