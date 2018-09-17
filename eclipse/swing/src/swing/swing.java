75e7546e1b9107bd2a50ecebefa2f5c41f8492d3
Quentin Luppo <quentinluppo@orange.fr>
2018-09-17 16:28:58 +0200

    day3

--- eclipse/swing/src/swing/swing.java
+++ eclipse/swing/src/swing/swing.java
@@ -1,5 +1,16 @@
 package swing;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class swing {
-
+	public static void main(String[] args) {
+	JFrame frame = new JFrame("HelloWorldSwing");
+	frame.setDefaultCloseOperation(
+	JFrame.EXIT_ON_CLOSE
+	);
+	JLabel label = new JLabel("Hello World");
+	frame.getContentPane().add(label);
+	frame.pack();
+	frame.setVisible(true);
+	}
 }
