56d62e95d32e7d5ee25c2f90fce7d7e5ffb291f9
pierre nolot <pnolot@orange.fr>
2018-09-17 09:50:13 +0200

    update branch

---
+++ Day2/FIFO_wo_object/FIFO_wo_object.java
@@ -0,0 +1,21 @@
+
+public class FIFO_wo_object {
+	int[] elements;
+	int size;
+	
+	public static int enfile(int val, int tab[]) {
+		if (tab == 0) {
+			int file[0] = val 
+		} else {
+			int[tab.length + 1] file;
+			file[file.length]=val;
+		}
+	}
+	
+	public static int pop(Fifo liste) {
+		int issou = f.elements[0];
+		size = size-1;
+		
+		return liste.elements[0];
+	}
+}
