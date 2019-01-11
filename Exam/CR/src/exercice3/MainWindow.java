e3b539a5f6a08745a3dc7489d57d4b729fa182ba
Clement <clement.mallejac.83@gmail.com>
2019-01-11 12:07:22 +0100

    CR

---
+++ Exam/CR/src/exercice3/MainWindow.java
@@ -0,0 +1,80 @@
+package exercice3;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import exercice3.model.Grid;
+import exercice3.view.CellWidget;
+import exercice3.view.GridWidget;
+
+public class MainWindow extends JFrame {
+
+	private static final Dimension PREFERED_SIZE = new Dimension(24,  24);
+	private static Image SMILEY;
+	
+	private Grid model;
+	
+	public MainWindow(Grid model) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("MineSweeper");
+		
+		this.model = model;
+		
+		JPanel buttonBar = new JPanel();
+		JButton smiley = new JButton(new ImageIcon(SMILEY));
+		smiley.setMargin(new Insets(2, 2, 2, 2));
+		smiley.addActionListener(e -> model.reset());
+		buttonBar.add(smiley);
+		
+		GridWidget grid = new GridWidget(model);
+		
+		add(buttonBar, BorderLayout.NORTH);
+		add(grid, BorderLayout.CENTER);
+		
+	}
+
+	public static void main(String[] args) {
+		Grid model = new Grid(4, 4);
+		// Initializing manually the grid
+		int b = 4, nbrLignes = 4, nbrColonnes = 4, min = 0;
+		for(int i = 0; i < b; ++i)
+		{
+			int l = min + (int)(Math.random()*((nbrLignes - min) ));
+			int c = min + (int)(Math.random()*((nbrColonnes - min) ));
+			model.cells[l][c].isBomb = true;
+		}
+
+		// Laying bombs
+		/*model.cells[0][1].isBomb = true;
+		model.cells[1][1].isBomb = true;
+		model.cells[2][1].isBomb = true;
+		model.cells[2][0].isBomb = true;*/
+		// Initializing neighbors
+		model.computeNeighboringBomb();
+		
+		MainWindow main = new MainWindow(model);
+		
+		main.pack();
+		main.setVisible(true);
+	}
+	
+	static {
+		try {
+			BufferedImage buffImg = ImageIO.read(MainWindow.class.getClassLoader().getResource("exercice3/smiley.png"));
+			SMILEY = buffImg.getScaledInstance(PREFERED_SIZE.width, PREFERED_SIZE.height, Image.SCALE_SMOOTH);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Exam/CR/src/exercice3/view/CellWidget.java
@@ -0,0 +1,116 @@
+package exercice3.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.border.BevelBorder;
+import javax.swing.plaf.metal.MetalButtonUI;
+
+import exercice3.model.Cell;
+import exercice3.model.CellState;
+
+public class CellWidget extends JButton implements Observer {
+
+	private static final Dimension PREFERED_SIZE = new Dimension(24,  24);
+	private static Image MINE;
+	private static Image FLAG;
+	
+	private final Cell model;
+	private boolean flagSet = false;
+	
+	public CellWidget(Cell model) {
+		this.model = model;
+		this.model.addObserver(this);
+		
+		setMinimumSize(PREFERED_SIZE);
+		setPreferredSize(PREFERED_SIZE);
+		
+		addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (!CellWidget.this.isEnabled()) return;
+				if (model.getState() == CellState.TRIGGERED) return;
+				if (e.getButton() == MouseEvent.BUTTON1) {
+					if (!flagSet) {
+						model.setState(CellState.TRIGGERED);
+					}
+		        } else if (e.getButton() == MouseEvent.BUTTON3) {
+		        	if (flagSet) {
+		        		setIcon(null);
+		        		flagSet = false;
+		        	}
+		        	else {
+		        		setIcon(new ImageIcon(FLAG));
+		        		flagSet = true;
+		        	}
+		          
+		        }
+		      }
+		});
+	}
+
+	private void stateChanged(CellState state) {
+		if (state == CellState.TRIGGERED) {
+			setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+			setEnabled(false);
+			
+			if (model.isBomb) {
+					setIcon(new ImageIcon(MINE));
+					setDisabledIcon(new ImageIcon(MINE));
+				
+			}
+			else {
+				if (model.nbOfNeiboringBomb>0) {
+					setText(String.valueOf(model.nbOfNeiboringBomb));
+					final Color c = Constants.COLOR_MAP.get(model.nbOfNeiboringBomb);
+					setUI(new MetalButtonUI() {
+						@Override
+				         protected Color getDisabledTextColor() {
+				            return c;
+				         }
+					});
+				}
+			}
+		}
+		else {
+			setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+			setEnabled(true);
+			if (model.isBomb) {
+				setIcon(null);
+			}
+			else {
+				setText("");
+			}
+			
+		
+		}
+
+	}
+	
+	@Override
+	public void update(Observable arg0, Object state) {
+		stateChanged((CellState) state);
+	}
+	
+	static {
+		try {
+			BufferedImage buffImg = ImageIO.read(CellWidget.class.getClassLoader().getResource("exercice3/mine.png"));
+			MINE = buffImg.getScaledInstance(PREFERED_SIZE.width, PREFERED_SIZE.height, Image.SCALE_SMOOTH);
+			buffImg = ImageIO.read(CellWidget.class.getClassLoader().getResource("exercice3/flag.jpg"));
+			FLAG = buffImg.getScaledInstance(PREFERED_SIZE.width, PREFERED_SIZE.height, Image.SCALE_SMOOTH);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
